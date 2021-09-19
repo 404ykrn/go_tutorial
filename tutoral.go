74b8ce3c4fc165edf33d38235cf2501a04535688
404ykrn <stu.7.kuzu.2@gmail.com>
2021-09-19 17:47:19 +0900

    commit about packages

---
+++ tutoral.go
@@ -0,0 +1,17 @@
+// A Tour of Go - Packages
+
+package main // main パッケージの宣言
+
+import ( // パッケージのインポート
+	"fmt" // Go 標準書式設定機能パッケージ
+	"math/rand" // Go 標準擬似乱数生成パッケージ
+)
+
+func main( ) { // main 関数の定義、引数は取らない
+    println ("Hello, Go!") // suffix が ln の print は要素間にスペースが入る、改行される
+}
+
+func main( ) {
+	fmt.Println("My favorite number is", rand.Intn(10)) // Int 型の値 n に 1 - 10 を渡す (1 回実行なので 1 が入る)
+}
+
