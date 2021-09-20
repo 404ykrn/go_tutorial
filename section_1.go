f6ad2bb46ddb23ee71da18148a789aad3f177c8b
404ykrn <stu.7.kuzu.2@gmail.com>
2021-09-20 13:26:57 +0900

    fix

--- section_1.go
+++ section_1.go
@@ -3,12 +3,16 @@
 package main  // main パッケージの宣言
 
 import ( // パッケージのインポート
-	"fmt"       // Go 標準書式設定機能パッケージ
-	"time"
+	"fmt"  // Go 標準書式設定機能パッケージ
+	"time" // Go 標準時間フォーマットパッケージ
 )
 
-func main() {
-	fmt.Println("Welcome to the playground!")
+func main( ) { // main 関数の定義
+	fmt.Println("Welcome to the playground!") // suffix が ln の print は要素間にスペースが入る、改行される
 
-	fmt.Println("The time is", time.Now())
+	fmt.Println("The time is", time.Now()) // time.now() で時刻を出力する
 }
+
+// output
+// Welcome to the playground!
+// The time is 2009-11-10 23:00:00 +0000 UTC m=+0.000000001
