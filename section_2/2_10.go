57ccd8199b62e63655a2d737855af4a48b5cdfab
404ykrn <stu.7.kuzu.2@gmail.com>
2021-09-21 19:51:06 +0900

    read 2_10

---
+++ section_2/2_10.go
@@ -0,0 +1,21 @@
+// section 2_10
+
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	// 変数として i , j にそれぞれ 1 , 2 を宣言
+	k := 3
+	// 2_6 に記載した省略記法を使用し、変数として k = 3 を宣言
+	c, python, java := true, false, "no!"
+	// 省略記法を使用し、変数として順番に true , false , "no!" を宣言
+	// 省略記法は関数の外では使用することができないので var で変数を宣言する必要がある
+
+	fmt.Println(i, j, k, c, python, java)
+}
+
+/* output
+ 1 2 3 true false no!
+*/
