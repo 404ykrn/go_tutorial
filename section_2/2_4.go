7e0255ce9d0e0cc9517dc041028bb4ef8e289ed0
404ykrn <stu.7.kuzu.2@gmail.com>
2021-09-21 09:25:47 +0900

    read 2_4

---
+++ section_2/2_4.go
@@ -0,0 +1,21 @@
+// section 2_4
+
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+// Go 言語の関数 : func 関数名(引数 引数の型) 返り値の型 { 処理 }
+// add 関数は int 型の 2 つのパラメータをとる
+	return x + y
+	// 返り値である変数に名前をつけることも可能
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+	// add 関数を呼び出して値を演算する
+}
+
+/* output
+ 55
+ */
