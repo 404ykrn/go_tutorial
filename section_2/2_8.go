e03a2b6fea21569b5abd0929091cd3a484a60bac
404ykrn <stu.7.kuzu.2@gmail.com>
2021-09-21 16:51:53 +0900

    read 2_8

---
+++ section_2/2_8.go
@@ -0,0 +1,33 @@
+// section 2_8
+
+package main
+
+import "fmt"
+
+var c, python, java bool
+// 変数として c, python, java を宣言し、型を bool 型に指定
+
+func main() {
+	var i int
+    // 変数として i を宣言、int 型に指定
+	fmt.Println(i, c, python, java)
+	// 指定された型通りに戻り値を出力
+}
+
+/* 型のデフォルト値について
+var 変数名 型 = hoge ではなく
+var 変数名 型 しか指定していないため、デフォルト値が返される
+デフォルト値は型によって決まっている
+---
+int  : 0
+bool : false
+str  : 空文字
+---
+*/
+
+/* output
+0 false false false
+*/
+
+
+
