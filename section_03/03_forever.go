925a148c77555bb57bd98e0ce9d85131eb720f33
404ykrn <stu.7.kuzu.2@gmail.com>
2021-10-02 20:38:31 +0900

    read 03_03

---
+++ section_03/03_forever.go
@@ -0,0 +1,15 @@
+// section 03 Forever
+// https://go-tour-jp.appspot.com/flowcontrol/4
+package main
+
+func main() {
+	for {
+	// for を記載して、条件文を省略して記載することで無限ループとなる
+	// ループをコンパクトに表現できる
+	}
+}
+
+/* output
+ timeout running program
+ // infinite loop
+*/
